667d17f3dd7d18a5cf5a4f710337be274e1e4bbd
KingMaj0r <abdurrahman@barakat.email>
2025-01-21 10:30:09 +0700

    Added nix support

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "File manager library that provides all the essentials.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      packages.libfm = pkgs.stdenv.mkDerivation rec {
+        pname = "libfm";
+        version = "1.0.0";
+
+        src = ./.;
+
+        nativeBuildInputs = [ pkgs.meson pkgs.ninja pkgs.pkg-config ];
+
+        buildInputs = [ pkgs.gcc pkgs.libcxx ];
+
+        meta = with pkgs.lib; {
+          description = "File manager library that provides all the essentials.";
+          license = licenses.mit;
+          platforms = platforms.linux;
+        };
+      };
+    });
+}
